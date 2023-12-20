1f7f8813a7605b0bb05c97e942ed6e53dc6c82a4
Spencerberry55 <123300853+Spencerberry55@users.noreply.github.com>
2023-12-20 13:51:12 +0000

    Create Validator - Spencer.py
    Validates the date of an event.
    
    It compares todays date before outputting a relevant message.

---
+++ Validator - Spencer.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+from dateutil import parser
+# Gets todays date
+today = datetime.today()
+# Puts it into the dd/mm/yyyy format
+today = today.strftime("%d/%m/%Y")
+
+
+def DateValidation():
+    eventDate = input(".")
+    # Makes sure any invalid dates are inputted
+    while True:
+        try:
+            print(bool(parser.parse(eventDate)))
+            return True
+        # Doesnt work if invalid
+        except ValueError:
+            print("Invalid Date")
+            return False
+
+    while True:
+        # Checks if date is before or after today
+        # If its after or is today, it allows the user to store the date
+        # If before today, it wont store it since it has already happened.
+            if eventDate > today:
+                print("Invalid date. Event has already happened")
+                return False
+            elif eventDate < today:
+                print("Event will happen soon.")
+                break
+            else:
+                print("The event is today")
+                break
+DateValidation()
