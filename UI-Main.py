a13fb10dd1178aa4cf369f92a576c3252eca0171
tombrqnd <154525919+tombrqnd@users.noreply.github.com>
2023-12-21 14:23:36 +0000

    Update UI-Main.py
    I have added some of the UI.

--- UI-Main.py
+++ UI-Main.py
@@ -1,1 +1,16 @@
 # Imports
+from tkinter import *
+
+root = Tk()
+
+view_events = Button(root, text="View Event")
+view_events.pack()
+create_events = Button(root, text="Create Event)
+create_events.pack()
+delete_events = Button(root, text="Delete Event")
+delete_events.pack()
+
+root.geometry("250x100")
+
+root.mainloop()
+
