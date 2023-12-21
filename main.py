ca570e43f2a69e773e635074de76f9cbf4b65ff5
OliverKoz <154432439+OliverKoz@users.noreply.github.com>
2023-12-21 14:22:13 +0000

    Working on Main
    I have added some changes, they will need to be improved in the next sprint

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# Imports
+import eventHandler
+import os
+
+
+# This will display the current menu
+
+events = {}
 def display_menu():
     print("\n===== Event Manager Menu =====")
     print("1. View Events")
@@ -6,23 +14,31 @@ def display_menu():
     print("4. Exit")
 
 def main():
-    events = []
-
     while True:
         display_menu()
         choice = input("Enter your choice (1-4): ")
 
         if choice == "1":
-            view_events(events)
+            print("Choice 1")
         elif choice == "2":
-            add_event(events)
+            print("Choice 2")
         elif choice == "3":
-            delete_event(events)
+            print("Choice 3")
         elif choice == "4":
             print("Exiting Event Manager. Goodbye!")
             break
         else:
             print("Invalid choice. Please enter a number between 1 and 4.")
 
+# Perform actions at code running
 if __name__ == "__main__":
+    if os.path.isdir("data"):
+        if os.path.isdir("data/jsonData"):
+            print(True)
+        else:
+            print(False)
+    else:
+        print(False)
+        pass
+
     main()
